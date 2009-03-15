321ccc24c01d1216bc53d5bd65d9b63aee89ad49
usarskyy <usarskyy@4def3a5e-fed8-11dd-a3ef-1f8437fb274a>
2009-03-15 19:16:52 +0000

    Added overloaded methods which have additional "userState" parameter

---
+++ trunk/dev/BoxSync.Core/Primitives/PublicShareResponse.cs
@@ -0,0 +1,17 @@
+﻿using BoxSync.Core.Statuses;
+
+
+namespace BoxSync.Core.Primitives
+{
+	public class PublicShareResponse : ResponseBase<PublicShareStatus>
+	{
+		/// <summary>
+		/// Unique identifier of a publicly shared object
+		/// </summary>
+		public string PublicName
+		{
+			get; 
+			set;
+		}
+	}
+}
