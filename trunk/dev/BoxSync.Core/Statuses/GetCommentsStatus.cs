a6c7e7fe8d9ad05f8c96f9850c09ff8954c0e7d4
usarskyy <usarskyy@4def3a5e-fed8-11dd-a3ef-1f8437fb274a>
2010-07-11 22:07:36 +0000

    Added "GetComments" method

--- trunk/dev/BoxSync.Core/Statuses/GetCommentsStatus.cs
+++ trunk/dev/BoxSync.Core/Statuses/GetCommentsStatus.cs
@@ -1,11 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace BoxSync.Core.Statuses
+﻿namespace BoxSync.Core.Statuses
 {
+	/// <summary>
+	/// Specifies statuses of 'get_comments' web method
+	/// </summary>
 	public enum GetCommentsStatus : byte
 	{
+		/// <summary>
+		/// Unknown status string
+		/// </summary>
+		Unknown = 0,
+
+		/// <summary>
+		/// Represents 'listing_ok' status string
+		/// </summary>
+		Successful = 1,
+
+		/// <summary>
+		/// An error ocured during operation execution
+		/// </summary>
+		Failed = 2,
 	}
 }
